d692b5f92e5a5c08002c01ec78b2ca8f0dbcc219
Allan Alexandre <alexandesigner@gmail.com>
2019-12-25 21:11:29 -0300

    Update versions and fixes bugs alerts

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,17 +1,13 @@
 // Configuration for your app
-const path = require('path')
+const path = require("path");
 
-module.exports = function (ctx) {
+module.exports = function(ctx) {
   return {
-    boot: [
-      'meteor'
-    ],
-    css: [
-      'app.styl'
-    ],
+    boot: ["meteor"],
+    css: ["app.styl"],
     extras: [
-      'roboto-font',
-      'material-icons' // optional, you are not bound to it
+      "roboto-font",
+      "material-icons" // optional, you are not bound to it
       // 'ionicons-v4',
       // 'mdi-v3',
       // 'fontawesome-v5',
@@ -20,29 +16,28 @@ module.exports = function (ctx) {
     supportIE: false,
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'history',
+      vueRouterMode: "history",
       // vueCompiler: true,
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
-      extendWebpack (cfg, { isServer, isClient }) {
+      extendWebpack(cfg, { isServer, isClient }) {
         cfg.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /node_modules/
-        })
+        });
         cfg.resolve.alias = {
           ...cfg.resolve.alias,
-          'api': path.resolve(__dirname, './api/imports/api')
-        }
-        cfg.externals = function (context, request, callback) {
-          var match = request.match(/^meteor\/(.+)$/)
-          var pack = match && match[1]
-          var locator = pack && 'Package["' + pack + '"]'
-          return locator ? callback(null, locator) : callback()
-        }
-
+          api: path.resolve(__dirname, "./api/imports/api")
+        };
+        cfg.externals = function(context, request, callback) {
+          var match = request.match(/^meteor\/(.+)$/);
+          var pack = match && match[1];
+          var locator = pack && 'Package["' + pack + '"]';
+          return locator ? callback(null, locator) : callback();
+        };
       }
     },
     devServer: {
@@ -53,33 +48,27 @@ module.exports = function (ctx) {
     // framework: 'all' --- includes everything; for dev only!
     framework: {
       components: [
-        'QLayout',
-        'QHeader',
-        'QDrawer',
-        'QPageContainer',
-        'QPage',
-        'QToolbar',
-        'QToolbarTitle',
-        'QBtn',
-        'QBtnGroup',
-        'QIcon',
-        'QList',
-        'QItem',
-        'QItemSection',
-        'QItemLabel',
-        'QInput',
-        'QCard',
-        'QDialog'
+        "QLayout",
+        "QHeader",
+        "QDrawer",
+        "QPageContainer",
+        "QPage",
+        "QToolbar",
+        "QToolbarTitle",
+        "QBtn",
+        "QBtnGroup",
+        "QIcon",
+        "QList",
+        "QItem",
+        "QItemSection",
+        "QItemLabel",
+        "QInput",
+        "QCard",
+        "QDialog"
       ],
-      directives: [
-        'Ripple',
-        'CloseDialog'
-      ],
+      directives: ["Ripple"],
       // Quasar plugins
-      plugins: [
-        'Notify',
-        'Dialog'
-      ]
+      plugins: ["Notify", "Dialog"]
       // iconSet: ctx.theme.mat ? 'material-icons' : 'ionicons'
       // i18n: 'de' // Quasar language
     },
@@ -92,38 +81,38 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {},
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar-PWA',
-        description: 'Best PWA App in town!',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        name: "Quasar App",
+        short_name: "Quasar-PWA",
+        description: "Best PWA App in town!",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
+            src: "statics/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
+            src: "statics/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
+            src: "statics/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
+            src: "statics/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
+            src: "statics/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       }
@@ -133,26 +122,23 @@ module.exports = function (ctx) {
     },
     electron: {
       // bundler: 'builder', // or 'packager'
-      extendWebpack (cfg) {
+      extendWebpack(cfg) {
         // do something with Electron process Webpack cfg
       },
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Window only
         // win32metadata: { ... }
       },
       builder: {
         // https://www.electron.build/configuration/configuration
-
         // appId: 'quasar-app'
       }
     }
-  }
-}
+  };
+};
